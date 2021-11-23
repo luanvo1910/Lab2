353b68677ef0435812e476de9699665b1648cf5b
luan <oga.tatsumi1230410@gmail.com>
2021-11-23 08:12:31 +0700

    Special number

---
+++ Special Number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Special_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number: ");
+            int n = int.Parse(Console.ReadLine());
+            for (var i = 1; i <= n; i ++)
+            {
+                int a, b, c, sum;
+                a = i / 100;
+                b = (i % 100) / 10;
+                c = i % 10;
+                sum = a + b + c;
+                if (sum == 5 || sum == 7 ||sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
